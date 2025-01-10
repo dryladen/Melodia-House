2c6713692b9a47fdf1bc481821dcc329012a6e8e
Laden <delfanladen24@gmail.com>
2025-01-10 14:12:53 +0800

    fix sidebar error when display in mobile side

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,22 +1,28 @@
 import AppNavbar from "@/components/app-navbar";
 import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { directus } from "@/lib/directus";
 import { readMe } from "@directus/sdk";
 import { cookies } from "next/headers";
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const token = (await cookies()).get("directus_session_token")?.value;
   const result = await directus(token).request(readMe({ fields: ["email"] }));
   return (
     <SidebarProvider>
       <AppSidebar email={result.email} />
-      <main className="w-full">
-        <div className="flex p-4 border-b-[1px] w-full">
-          <AppNavbar />
-        </div>
-        <div className="p-4">{children}</div>
-      </main>
+      <SidebarInset>
+        <main className="w-full">
+          <div className="flex p-4 border-b-[1px] w-full">
+            <AppNavbar />
+          </div>
+          <div className="p-4">{children}</div>
+        </main>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
