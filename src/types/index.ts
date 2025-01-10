521e81444efbce51286f7099c8126d679b7b33bc
Laden <delfanladen24@gmail.com>
2025-01-11 00:08:37 +0800

    refactor (types) : add more key in intruments type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,22 @@
 export type Instrument = {
   id: number;
   name: string;
-  students : string[];
-  teachers : string[];
+  students: {
+    id: number;
+    students_id?: {
+      id: number;
+      first_name: string;
+      last_name: string;
+      email: string;
+    };
+  }[];
+  teachers: {
+    id: number;
+    students_id?: {
+      id: number;
+      first_name: string;
+      last_name: string;
+      email: string;
+    };
+  }[];
 };
