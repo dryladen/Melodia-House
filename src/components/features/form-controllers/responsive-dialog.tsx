84575715b312a875bf08dfbb20dc4537ab87503a
Laden <delfanladen24@gmail.com>
2025-01-10 13:42:42 +0800

    Create responsive dialog

---
+++ src/components/features/form-controllers/responsive-dialog.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+export function ResponsiveDialog({
+  children,
+  isOpen,
+  setIsOpen,
+  title,
+  description,
+}: {
+  children: React.ReactNode;
+  isOpen: boolean | undefined;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>> | undefined;
+  title: string;
+  description?: string;
+}) {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer open={isOpen} onOpenChange={setIsOpen}>
+        <DrawerContent className="p-4">
+          <DrawerHeader className="text-left px-0 py-4">
+            <DrawerTitle>{title}</DrawerTitle>
+            {description && (
+              <DialogDescription>{description}</DialogDescription>
+            )}
+          </DrawerHeader>
+          {children}
+          <DrawerFooter className="pt-2 px-0">
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          {description && <DialogDescription>{description}</DialogDescription>}
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
