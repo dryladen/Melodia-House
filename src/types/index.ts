9f6fbab242c725c439ca8581526b7bb2e17b36f6
Laden <delfanladen24@gmail.com>
2025-01-11 15:10:59 +0800

    feat ( types ) : add package type

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,46 @@ export type Instrument = {
     };
   }[];
 };
+
+// id: 11458,
+// status: 'draft',
+// user_created: '12cb980d-7e07-4bfb-b30e-7a083aea6b48',
+// date_created: '2024-12-22T08:48:16.199Z',
+// user_updated: null,
+// date_updated: null,
+// name: 'Standard',
+// start_datetime: '2024-05-20T04:11:11.103Z',
+// end_datetime: '2024-10-18T09:50:51.305Z',
+// student: '2ec086e7-7587-491c-bd13-9e30f9f7baa6',
+// duration: 90,
+// remarks: null,
+// instrument: 9,
+// lessons_quota: 18,
+// lessons: [
+//   432791, 432792, 432793,
+//   432794, 432795, 432796,
+//   432797, 432798, 432799,
+//   432800, 432801, 432802,
+//   432803, 432804, 432805,
+//   432806, 432807, 432808
+// ],
+// payments: [ 11055 ]
+export type Package = {
+  id: number;
+  status: string;
+  name: string;
+  duration: number;
+  start_datetime: string;
+  end_datetime: string;
+  student: {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+  };
+  instrument: {
+    id: number;
+    name: string;
+  };
+  lessons: {};
+};
