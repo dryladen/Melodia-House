c6a965a07623387605c29d0d119936e23e7d9243
Laden <delfanladen24@gmail.com>
2025-01-12 09:54:57 +0800

    feat ( authentication ) : implement simple authentication

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -4,23 +4,28 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { directus } from "@/lib/directus";
 import { readMe } from "@directus/sdk";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const token = (await cookies()).get("directus_session_token")?.value;
-  const result = await directus(token).request(readMe({ fields: ["email"] }));
-  return (
-    <SidebarProvider>
-      <AppSidebar email={result.email} />
-      <SidebarInset className="min-h-screen">
-        <div className="flex p-4 border-b-[1px] w-full">
-          <AppNavbar />
-        </div>
-        <div className="p-4 bg-muted flex flex-col grow">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  try {
+    const token = (await cookies()).get("directus_session_token")?.value;
+    const result = await directus(token).request(readMe({ fields: ["email"] }));
+    return (
+      <SidebarProvider>
+        <AppSidebar email={result.email} />
+        <SidebarInset className="min-h-screen">
+          <div className="flex p-4 border-b-[1px] w-full">
+            <AppNavbar />
+          </div>
+          <div className="p-4 bg-muted flex flex-col grow">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    );
+  } catch (error) {
+    redirect("/login");
+  }
 }
